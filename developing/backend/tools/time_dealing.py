d59b47040875886fa0ba093baae420f3cdfa5328
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-07-13 18:59:06 -0300

    time_dealing OK

--- developing/backend/tools/time_dealing.py
+++ developing/backend/tools/time_dealing.py
@@ -13,11 +13,44 @@ class Time:
         self.MINUTES = MIN
         self.SECONDS = SEC
 
+
     def __add__(self, other):
-        pass
+        H, M, S = 0
+
+        S += self.SECONDS + other.SECONDS
+        if S >= 60:
+            S -= 60
+            M += 1
+        
+        M += self.MINUTES + other.MINUTES
+        if M >= 60:
+            M -= 60
+            H += 1
+        
+        H += self.HOURS + other.HOURS
+
+        return Time(f"{H}:{M}:{S}")
+
 
     def __sub__(self, other):
-        pass
+        H, M, S = 0, 0, 0
+        
+        S = self.SECONDS - other.SECONDS
+        if S < 0:
+            S += 60
+            M -= 1
+        
+        M += self.MINUTES - other.MINUTES
+        if M < 0:
+            M += 60
+            H -= 1
+        
+        H += self.HOURS - other.HOURS
+        
+        if H < 0: raise ValueError("Resultado da subtração não pode ser negativo.")
+        
+        return Time(f"{H}:{M}:{S}")
+
 
     def __mul__(self, other):
         raise("Essa classe não suporta multiplicação.")
@@ -25,3 +58,5 @@ class Time:
     def __truediv__(self, other):
         raise("Essa classe não suporta divisão.")
     
+    def __repr__(self):
+        return f"{self.HOURS}:{self.MINUTES}:{self.SECONDS}"
