20dd5c47d8ac21c3c5ffe755f74a5d3c48724822
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-07-17 01:02:26 -0300

    Acho q Progress ta OK

--- developing/backend/classes/progress.py
+++ developing/backend/classes/progress.py
@@ -3,36 +3,51 @@ from tools.time_dealing import Time, Seconds_to_Time
 class Progress():
     def __init__(self, TYPE):
         self.INDEX = -1
-        self.PROGRESS = -1
+        self.PROGRESS = -1  # Only payloads have "progress" percentage
+        self.TYPE = TYPE
+        self.TARGET = None
         
-        if TYPE == ""
+        if self.TYPE == "payload_progress":
+            self.TARGET = Payload()
+        elif self.TYPE == "objective_updated":
+            self.TARGET = Objective()
+        
+        self.UpdateAll()
+    
+    def AddIndex(self):
+        self.TARGET.AddIndex()
+        self.UpdateAll()
+    
+    def UpdateProgress(self):
+        if self.TYPE == "payload_progress":
+            self.TARGET.UpdateProgress()
+        else:
+            raise("Esse tipo de objetivo não possui porcentagem de progresso.")
+        self.UpdateAll
+    
+    def UpdateAll(self):
+        self.INDEX = self.TARGET.PROGRESS_INDEX
+        if self.TYPE == "payload_progress":
+            self.PROGRESS = self.TARGET.PAYLOAD_PROGRESS
+            
 
 
 class Objective():
     def __init__(self):
-        self.OBJECTIVE_INDEX = 0 #checkpoints
+        self.PROGRESS_INDEX = 0 #checkpoints
     
     def AddIndex(self):
-        self.OBJECTIVE_INDEX += 1
-    
-    def GetIndex(self):
-        return self.OBJECTIVE_INDEX
+        self.PROGRESS_INDEX += 1
 
 
 
 class Payload():
     def __init__(self):
-        self.OBJECTIVE_INDEX = 0 #checkpoints
+        self.PROGRESS_INDEX = 0 #checkpoints
         self.PAYLOAD_PROGRESS = 0
     
     def AddIndex(self):
-        self.OBJECTIVE_INDEX += 1
-    
-    def GetIndex(self):
-        return self.OBJECTIVE_INDEX
+        self.PROGRESS_INDEX += 1
     
     def UpdateProgress(self, NEW_VALUE):
         self.PAYLOAD_PROGRESS = NEW_VALUE
-    
-    def GetProgress(self):
-        return self.PAYLOAD_PROGRESS
